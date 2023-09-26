f1381c225533aed49d2e8b486e07142f26743a6b
Andre Rodrigues <andre.rodrigues@giffits.de>
2023-09-26 06:40:42 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# quizzlerapp
+# Quizzler App
 Quizzler App - #100DaysOfCode - Day 34
---
+++ data.py
@@ -0,0 +1,11 @@
+import requests
+
+API_URL = 'https://opentdb.com/api.php'
+PARAMETERS = {
+    'amount': 10,
+    'type': 'boolean'
+}
+
+response = requests.get(url=API_URL, params=PARAMETERS)
+response.raise_for_status()
+question_data = response.json()['results']
